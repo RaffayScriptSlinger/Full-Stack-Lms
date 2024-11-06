5dcf1afa9f60c3f2fc9e1dc478a491d616a61d21
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-11-06 06:44:46 -0930

    Working

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,16 +1,20 @@
-import { connectToMongoDB } from "@/lib/dbConnect";
+import { connectDB } from "@/lib/dbConnect";
+import { userModel } from "@/lib/modals/userModal";
 
 export async function POST(request) {
-    await connectToMongoDB();
+    await connectDB();
 
     const obj = await request.json()
     console.log("obj => ",obj)
-    return Response.json("user Post Request")
 
 
+    const user = await userModel.findOne({email : obj.email})
+    console.log(user)
+    return Response.json("user Post Request Hogaye han")
 
 }
 export async function GET() {
     return Response.json("user Get  Request")
 
 }
+
--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -2,20 +2,15 @@
 
 import mongoose from "mongoose"
 
-export async function  connectToMongoDB() {
+export async function  connectDB() {
     try{
         let connection
         if(connection?.connection?.readyState != 1){
             connection = await mongoose.connect(process.env.MONGODB_URL);
             console.log("db Connect Hogaya");
-
-        }
-       
-    
+        }   
     }
     catch (err){
         console.log("err =>" , err)
-
     }
-   
 }
